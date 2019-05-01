e9747c82eddaee1cef06ac3ca0447e4ec7156698
indu <indu@indu-PC>
2019-05-01 11:08:34 +0530

    error is resolve

--- demoLog4j/src/main/java/com/it/controller/TestController.java
+++ demoLog4j/src/main/java/com/it/controller/TestController.java
@@ -10,12 +10,13 @@ public class TestController {
     
     private Logger logger = Logger.getLogger(TestController.class);
      
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    @RequestMapping( value="/test",method = RequestMethod.GET)
     public ModelAndView test() {
-        ModelAndView model = new ModelAndView("Hello");
-         System.out.println("inter");
-        logger.info("This is an info log entry");
+        ModelAndView model = new ModelAndView("hello");
+        
         logger.error("This is an error log entry");
+        logger.info("This is an info log entry");
+       //  model.setViewName("hello");
         
         return model;
     }
